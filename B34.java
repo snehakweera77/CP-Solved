469644743e8fd132c899073f29bdcf661cfa9da3
snehakweera77 <snehakweera77@gmail.com>
2021-08-15 22:10:06 +0530

    B34

---
+++ B34.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B34 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] nArr = new int[n];
+        for (int i = 0; i < n; i++)
+            nArr[i] = sc.nextInt();
+        Arrays.sort(nArr);
+        int ans = 0;
+        for (int i = 0; i < m; i++)
+            ans += nArr[i];
+        System.out.println(-ans);
+    }
+}
