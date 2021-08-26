34c3a40fe63b0ce16b8159822fc1585933d72b4e
snehakweera77 <snehakweera77@gmail.com>
2021-08-26 10:21:24 +0530

    Pongal Bunk - Codechef

--- CodechefCowa19b.java
+++ CodechefCowa19b.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-class contest {
+class CodechefCowa19b {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
